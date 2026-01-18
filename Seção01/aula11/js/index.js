b43279994128251ab66e19047266d745de1a7ff6
Adriano Felisberto <146196739+Adriano-Felisberto@users.noreply.github.com>
2026-01-18 12:01:23 -0300

    Delete Seção01 directory

--- Seção01/aula11/js/index.js
+++
@@ -1,9 +0,0 @@
-alert('Com a nossa mensagem. ') // é um atalho para window.alert
-
-window.confirm('Deseja realmente apagar? ') // com o window.confirm
-//usamos para confirmar algo vai retornar true ou false
-
-const nome = window.prompt('Digite seu nome: ') // com o window.prompt
-// usamos para que o usuario digite algo, retorna o valor digitado ou null se cancelar
-
-window.alert(nome)
